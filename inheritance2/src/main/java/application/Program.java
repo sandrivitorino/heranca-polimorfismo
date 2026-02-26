ed2982fe2dca6902403e5660153c7b9335988a5d
vonvit <sandrineviturn@gmail.com>
2026-02-26 19:42:08 -0300

    downcasting

--- inheritance2/src/main/java/application/Program.java
+++ inheritance2/src/main/java/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import entities.Account;
 import entities.BusinessAccount;
+import entities.SavingsAccount;
 
 public class Program {
 
@@ -13,5 +14,9 @@ public class Program {
         // UPCASTING
 
         Account acc1 = bacc;
+        Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+        Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+
+        // DOWNCASTING
     }
 }
