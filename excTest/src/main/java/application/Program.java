926d570901440c6e547e3895581316d32350fb0a
vonvit <sandrineviturn@gmail.com>
2026-02-28 17:48:10 -0300

    Polimorfismo

--- excTest/src/main/java/application/Program.java
+++ excTest/src/main/java/application/Program.java
@@ -8,7 +8,7 @@ public class Program {
 
     public static void main(String[] args) {
 
-        Account acc1 = new Account(1001, "Alex", 1000.00);
+        /*Account acc1 = new Account(1001, "Alex", 1000.00);
         acc1.withdraw(200.0);
         System.out.println(acc1.getBalance());
 
@@ -18,6 +18,17 @@ public class Program {
 
         Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
         acc3.withdraw(200.0);
-        System.out.println(acc3.getBalance());
+        System.out.println(acc3.getBalance());*/
+
+        //POLIMORFISMO
+
+        Account x = new Account(1020, "Alex", 1000.0);
+        Account y = new SavingsAccount(1023, "Maria", 1000.0, 0.01);
+
+        x.withdraw(50.0);
+        y.withdraw(50.0);
+
+        System.out.println(x.getBalance());
+        System.out.println(y.getBalance());
     }
 }
