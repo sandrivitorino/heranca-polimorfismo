51dd61be53117840de5c36a9988038c9d224bb34
vonvit <sandrineviturn@gmail.com>
2026-02-27 23:32:01 -0300

    finished

--- excTest/src/main/java/application/Program.java
+++ excTest/src/main/java/application/Program.java
@@ -15,5 +15,9 @@ public class Program {
         Account acc2 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
         acc2.withdraw(200.0);
         System.out.println(acc2.getBalance());
+
+        Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
+        acc3.withdraw(200.0);
+        System.out.println(acc3.getBalance());
     }
 }
