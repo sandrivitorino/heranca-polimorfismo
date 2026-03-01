ca289bb4ab316008c21b2fc5867a1e9da240ed36
vonvit <sandrineviturn@gmail.com>
2026-03-01 18:02:25 -0300

    add default program class.

--- inheritance4/src/main/java/application/Program.java
+++ inheritance4/src/main/java/application/Program.java
@@ -1,4 +1,20 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
+
+    void main(){
+
+        Locale.setDefault(Locale.US);
+        Scanner sc =  new Scanner(System.in);
+
+
+
+        sc.close();
+
+    }
+
+
 }
